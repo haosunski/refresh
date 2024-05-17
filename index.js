ecb4b9ec568c0701fca772d18cd15c4c4b9b7ac9
Hao Sun <wshadowhunter.cosmosvigorous@gmail.com>
2024-05-17 15:33:37 +0800

    second

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 //Make sure you have installed all the dependencies with "npm i".
 //The password is ILoveProgramming
 import express from "express";
-const app = express()
+const app = express();
 
 app.use(express.urlencoded({extended: true}))
 
